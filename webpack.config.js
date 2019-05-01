c583ab3b3fa8c292e3d1ebb34ff2d0fda4cd4586
Luiz Américo <luizamericop@gmail.com>
2019-04-30 23:02:58 -0300

    Update pdfkit to 0.9.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const StringReplacePlugin = require('string-replace-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   resolve: {
 		alias: {
-			fs: path.join(__dirname, './virtual-fs.js')
+			fs: 'pdfkit/js/virtual-fs.js'
 		}
   },
   module: {    
@@ -14,18 +13,6 @@ module.exports = {
       { enforce: 'post', test: /fontkit[/\\]index.js$/, loader: "transform-loader?brfs" },
       { enforce: 'post', test: /unicode-properties[/\\]index.js$/, loader: "transform-loader?brfs" },
       { enforce: 'post', test: /linebreak[/\\]src[/\\]linebreaker.js/, loader: "transform-loader?brfs" },
-      {
-				test: /fontkit[/\\]index.js$/, loader: StringReplacePlugin.replace({
-					replacements: [
-						{
-							pattern: /fs\./g,
-							replacement: function () {
-								return 'require(\'fs\').';
-							}
-						}
-					]
-				})
-			},
       { test: /src[/\\]assets/, loader: 'arraybuffer-loader'},
       { test: /\.afm$/, loader: 'raw-loader'}
     ]
